868525fd943fe1cc8ec7d2854c937e142c85eee0
Makenzie Adams <adamsm.realtor@gmail.com>
2021-09-18 19:18:12 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, word) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${word} gift!`);
+  }
+
+  return messages;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number--);
+  }
+}
